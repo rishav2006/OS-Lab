27c49114052ace8550503eda5bc9825a8e636121
Rishav Pattnaik <71424136+rishav2006@users.noreply.github.com>
2026-03-11 15:50:58 +0530

    Update 3.c

--- Assignment_09/3.c
+++ Assignment_09/3.c
@@ -10,26 +10,21 @@ void graph3() {
     printf("========================================\n");
 
     printf("P1 (PID: %d)\n", getpid());
-    printf("P1 (PID: %d)\n", getpid());
 
     pid_t p2 = fork();
     if (p2 == 0) {
-        printf("P2 (PID: %d)\n", getpid());
         printf("P2 (PID: %d)\n", getpid());
 
         pid_t p4 = fork();
         if (p4 == 0) {
             printf("P4 (PID: %d)\n", getpid());
-            printf("P4 (PID: %d)\n", getpid());
 
             pid_t p7 = fork();
             if (p7 == 0) {
                 printf("P7 (PID: %d)\n", getpid());
-                printf("P7 (PID: %d)\n", getpid());
 
                 pid_t p9 = fork();
                 if (p9 == 0) {
-                    printf("P9 (PID: %d)\n", getpid());
                     printf("P9 (PID: %d)\n", getpid());
                     _exit(0);
                 }
@@ -43,7 +38,6 @@ void graph3() {
         pid_t p5 = fork();
         if (p5 == 0) {
             printf("P5 (PID: %d)\n", getpid());
-            printf("P5 (PID: %d)\n", getpid());
             _exit(0);
         }
 
@@ -54,16 +48,13 @@ void graph3() {
     pid_t p3 = fork();
     if (p3 == 0) {
         printf("P3 (PID: %d)\n", getpid());
-        printf("P3 (PID: %d)\n", getpid());
 
         pid_t p6 = fork();
         if (p6 == 0) {
             printf("P6 (PID: %d)\n", getpid());
-            printf("P6 (PID: %d)\n", getpid());
 
             pid_t p8 = fork();
             if (p8 == 0) {
-                printf("P8 (PID: %d)\n", getpid());
                 printf("P8 (PID: %d)\n", getpid());
                 _exit(0);
             }
